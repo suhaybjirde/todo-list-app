10221421553ce0ce6b26dff2ef3ce7331d9dfd71
Suhayb Jirde <dribblekayga@gmail.com>
2023-03-02 01:49:39 -0800

    add completed task functionality.

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,10 @@
 const root = document.querySelector(':root')
 const themeChenger = document.querySelector('.main__todo__header__img-wrapper')
 const bg = document.querySelector('.main__bg')
-console.log(themeChenger)
+const todoListUl = document.querySelector('.main__todo__list')
 
-function lightTheme() {
-    root.style.setProperty('--clr-neutral-100', 'hsl(236, 33%, 92%)')
-    root.style.setProperty('--clr-neutral-200', 'hsl(235, 24%, 19%)')
-    root.style.setProperty('--clr-neutral-300', 'hsl(234, 39%, 85%)')
-    root.style.setProperty('--clr-neutral-400', 'hsl(233, 14%, 35%)')
-    root.style.setProperty('--clr-neutral-500', 'hsl(234, 11%, 52%)')
-    root.style.setProperty('--clr-neutral-600', 'hsl(235, 21%, 11%)')
-    root.style.setProperty('--clr-neutral-700', 'hsl(237, 14%, 26%)')
-    root.style.setProperty('--clr-neutral-800', 'hsl(0, 0%, 0%)')
-}
+
+// themes
 
 function darkTheme() {
     root.style.setProperty('--clr-neutral-100', 'hsl(236, 33%, 92%)')
@@ -24,16 +16,6 @@ function darkTheme() {
     root.style.setProperty('--clr-neutral-700', 'hsl(237, 14%, 26%)')
     root.style.setProperty('--clr-neutral-800', 'hsl(0, 0%, 0%)')
 }
-// neutral: (
-//     100:hsl(236, 33%, 92%), /* todo and themechanger*/
-//     200:hsl(235, 24%, 19%), /* background l*/
-//     300:hsl(234, 39%, 85%), /* li font color */
-//     500:hsl(234, 11%, 52%), /* input links at the bottom*/
-//     600: hsl(235, 21%, 11%), /* main bg background color*/ 
-//     400:hsl(233, 14%, 35%), /* finished taskes */
-//     700:hsl(237, 14%, 26%),
-// )
-
 
 themeChenger.addEventListener('click', ()=> {
     themeChenger.classList.toggle('lightTheme')
@@ -57,14 +39,19 @@ themeChenger.addEventListener('click', ()=> {
     }
 })
 
+// completed task 
 
-function finished() {
-    let check = document.querySelectorAll('.main__todo__list__item__check');
-    check.forEach(check => {
-        check.addEventListener('click', ()=> {
-            check.classList.toggle('checked')
-        })
+completedTask()
+
+function completedTask() {
+    todoListUl.addEventListener('click', (e)=> {
+        target = e.target;
+        targetParent = e.target.parentElement;
+        targetParentsParent = e.target.parentElement.parentElement;
+        targetParentsParentsParent = e.target.parentElement.parentElement.parentElement;
+        if (target.classList.contains('main__todo__list__item__check')) targetParent.toggleAttribute('data-completed');
+        if (target.classList.contains('check')) targetParentsParent.toggleAttribute('data-completed');
+        if (target.classList.contains('fa-cleck')) targetParentsParentsParent.toggleAttribute('data-completed')
     })
 }
-
-finished()
+// 
