1f049094922e2e525f456df401cdc04da86814db
Suhayb Jirde <dribblekayga@gmail.com>
2023-03-02 02:01:16 -0800

    add the ability to create new task

--- app/js/script.js
+++ app/js/script.js
@@ -1,40 +1,43 @@
-const root = document.querySelector(':root')
-const themeChenger = document.querySelector('.main__todo__header__img-wrapper')
-const bg = document.querySelector('.main__bg')
-const todoListUl = document.querySelector('.main__todo__list')
+const root = document.querySelector(':root'),
+    themeChenger = document.querySelector('.main__todo__header__img-wrapper'),
+    bg = document.querySelector('.main__bg'),
+    todoListUl = document.querySelector('.main__todo__list'),
+    input = document.querySelector('#input'),
+    form = document.querySelector('#form');
+
 
 
 // themes
 
 function darkTheme() {
-    root.style.setProperty('--clr-neutral-100', 'hsl(236, 33%, 92%)')
-    root.style.setProperty('--clr-neutral-200', 'hsl(235, 24%, 19%)')
-    root.style.setProperty('--clr-neutral-300', 'hsl(234, 39%, 85%)')
-    root.style.setProperty('--clr-neutral-400', 'hsl(233, 14%, 35%)')
-    root.style.setProperty('--clr-neutral-500', 'hsl(234, 11%, 52%)')
-    root.style.setProperty('--clr-neutral-600', 'hsl(235, 21%, 11%)')
-    root.style.setProperty('--clr-neutral-700', 'hsl(237, 14%, 26%)')
-    root.style.setProperty('--clr-neutral-800', 'hsl(0, 0%, 0%)')
+    root.style.setProperty('--clr-neutral-100', 'hsl(236, 33%, 92%)');
+    root.style.setProperty('--clr-neutral-200', 'hsl(235, 24%, 19%)');
+    root.style.setProperty('--clr-neutral-300', 'hsl(234, 39%, 85%)');
+    root.style.setProperty('--clr-neutral-400', 'hsl(233, 14%, 35%)');
+    root.style.setProperty('--clr-neutral-500', 'hsl(234, 11%, 52%)');
+    root.style.setProperty('--clr-neutral-600', 'hsl(235, 21%, 11%)');
+    root.style.setProperty('--clr-neutral-700', 'hsl(237, 14%, 26%)');
+    root.style.setProperty('--clr-neutral-800', 'hsl(0, 0%, 0%)');
 }
 
 themeChenger.addEventListener('click', ()=> {
-    themeChenger.classList.toggle('lightTheme')
+    themeChenger.classList.toggle('lightTheme');
     if (themeChenger.classList.contains('lightTheme')) {
-        themeChenger.firstElementChild.classList.add('icon-animation')
+        themeChenger.firstElementChild.classList.add('icon-animation');
         setTimeout(()=>{
-            lightTheme()
+            lightTheme();
             themeChenger.innerHTML = '<i class="fa-solid fa-moon"></i>';
-            themeChenger.firstElementChild.classList.remove('icon-animation')
-            bg.classList.add('light')
+            themeChenger.firstElementChild.classList.remove('icon-animation');
+            bg.classList.add('light');
         }, 400)
     }
     else {
-        themeChenger.firstElementChild.classList.add('icon-animation')
+        themeChenger.firstElementChild.classList.add('icon-animation');
         setTimeout(()=>{
-            darkTheme()
-            themeChenger.firstElementChild.classList.remove('icon-animation')
-            themeChenger.innerHTML = '<i class="fa-solid fa-sun"></i>'
-            bg.classList.remove('light')
+            darkTheme();
+            themeChenger.firstElementChild.classList.remove('icon-animation');
+            themeChenger.innerHTML = '<i class="fa-solid fa-sun"></i>';
+            bg.classList.remove('light');
         }, 400)
     }
 })
@@ -54,4 +57,40 @@ function completedTask() {
         if (target.classList.contains('fa-cleck')) targetParentsParentsParent.toggleAttribute('data-completed')
     })
 }
-// 
+// add task
+addTask()
+
+function addTask() {
+    form.addEventListener('submit', (e)=> {
+        e.preventDefault()
+        let newTask = createTask(input.value)
+        console.log(newTask)
+        todoListUl.appendChild(newTask)
+    })
+}
+
+function createTask(taskName) {
+    let taskTemp = `
+    <li class="main__todo__list__item">
+        <button class="main__todo__list__item__check">
+            <div class="check">
+            <i class="fa-solid fa-check"></i>
+            </div>
+        </button>
+        <div class="main__todo__list__item__wrapper">
+            <p class="main__todo__list__item__wrapper__name">${taskName}</p>
+            <div class="main__todo__list__item__wrapper__icon-wrapper">
+            <button id="edit">
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button id="delete">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+            </div>
+        </div>
+    </li>
+    `
+    let template = document.createElement('template');
+    template.innerHTML = taskTemp.trim()
+    return template.content.firstElementChild;
+}
